5d256959032862ed0c7bb6d0e8d6b38c703b0a8b
VAN PHAT LE <phat.lv@myan.vn>
2019-08-27 13:50:09 +0700

    add file init theme atomui

---
+++ resources/views/includes/sidebar-brand.blade.php
@@ -0,0 +1,12 @@
+<div class="admin-sidebar-brand">
+    <!-- begin sidebar branding-->
+    <img class="admin-brand-logo" src="{{asset('/assets/img/logo.png')}}"  width="40" alt="atmos Logo">
+    <span class="admin-brand-content font-secondary"><a href='index.html'>  atmos</a></span>
+    <!-- end sidebar branding-->
+    <div class="ml-auto">
+        <!-- sidebar pin-->
+        <a href="#" class="admin-pin-sidebar btn-ghost btn btn-rounded-circle"></a>
+        <!-- sidebar close for mobile device-->
+        <a href="#" class="admin-close-sidebar"></a>
+    </div>
+</div>
